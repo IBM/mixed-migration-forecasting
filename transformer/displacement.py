3826c3f7a21cec41e16bd5cd001fb6dca2e7a9e3
Rahul Nair <raul.nair@gmail.com>
2019-11-07 17:59:27 +0000

    displacement

--- transformer/displacement.py
+++ transformer/displacement.py
@@ -1,54 +1,72 @@
-# %%
+from .base import Transformer
 import os
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
-DATAFOLDER = "../prm-datasets/indicators/"
-IDMC_DATA = "Displacement/idmc_displacement_all_dataset.xlsx"
-UNHCR_DATA = "Displacement/UNHCR_displacement.csv"
+class DisplacementTransformer(Transformer):
+    """
+    Generates the target forecast variables: Total external displacement and internal displacement
 
-# %%
-idmc = pd.read_excel(os.path.join(DATAFOLDER, IDMC_DATA), skiprows=[1])
+    Two sources:
+    UNHCR and IDMC
+    """
 
-idmc['idp'] = idmc['Conflict New Displacements'] + idmc['Disaster New Displacements']
-print("IDMC data source")
-print("Data range: {} -> {}".format(idmc.Year.min(), idmc.Year.max()))
-print("Data for {} countries.".format(len(idmc.ISO3.unique())))
-print("IDP volumes range: {} -> {}".format(idmc.idp.min(), idmc.idp.max()))
+    def __init__(self, source, target):
+        super().__init__(source, target)
 
-idmc = idmc.rename(columns={'ISO3': 'Country Code', 'Name': 'Country Name', 'Year': 'year', 'idp': 'value'})
+    def read(self):
+        """ Overloaded method, since we have multiple sources """
 
-idmc['Indicator Code'] = 'IDMC.IDP'
-idmc['Indicator Name'] = 'Internally displaced persons'
+        self.idmc = pd.read_excel(self.source[0], skiprows=[1])
 
-idmc = idmc[['Country Code', 'Country Name', 'year', 'Indicator Code', 'Indicator Name', 'value']]
-idmc.head()
+        self.unhcr = pd.read_csv(self.source[1],
+                                usecols=['Origin', 'iso3', 'Year', 'variable', 'value'])
 
-# %%
-unhcr = pd.read_csv(os.path.join(DATAFOLDER, UNHCR_DATA), 
-        usecols=['Origin', 'iso3', 'Year', 'variable', 'value'])
+    def __unhcr_transformer(self):
+        """ UNHCR externally displaced populations """
 
-# Ignore IDP numbers from unhcr
-c1 = unhcr.variable.isin(['Refugees', 'Asylum.seekers', 'Others.of.concern'])
-unhcr = unhcr[c1]
+        # Ignore IDP numbers from unhcr
+        c1 = self.unhcr.variable.isin(['Refugees', 'Asylum.seekers', 'Others.of.concern'])
+        self.unhcr = self.unhcr[c1]
 
-# Sum things up
-unhcr = pd.pivot_table(unhcr, values='value', 
-        index = ['Origin', 'iso3', 'Year'], aggfunc=np.sum)
-unhcr.reset_index(inplace=True)
+        # Sum things up
+        self.unhcr = pd.pivot_table(self.unhcr, values='value', 
+                index = ['Origin', 'iso3', 'Year'], aggfunc=np.sum)
+        self.unhcr.reset_index(inplace=True)
 
-print("UNHCR source")
-print("Data range: {} -> {}".format(unhcr.Year.min(), unhcr.Year.max()))
-print("Data for {} countries.".format(len(unhcr.iso3.unique())))
-print("'External displacement' volumes range: {} -> {}".format(unhcr.value.min(), unhcr.value.max()))
+        # print("UNHCR source")
+        # print("Data range: {} -> {}".format(unhcr.Year.min(), unhcr.Year.max()))
+        # print("Data for {} countries.".format(len(unhcr.iso3.unique())))
+        # print("'External displacement' volumes range: {} -> {}".format(unhcr.value.min(), unhcr.value.max()))
 
-unhcr = unhcr.rename(columns={'Origin': 'Country Name', 'iso3': 'Country Code', 'Year': 'year'})
-unhcr['Indicator Code'] = 'UNHCR.EDP'
-unhcr['Indicator Name'] = 'UNHCR total externally displaced persons'
-unhcr.head()
-# %%
+        self.unhcr = self.unhcr.rename(columns={'Origin': 'Country Name', 'iso3': 'Country Code', 'Year': 'year'})
+        self.unhcr['Indicator Code'] = 'UNHCR.EDP'
+        self.unhcr['Indicator Name'] = 'UNHCR total externally displaced persons'
 
-df = pd.concat([unhcr, idmc], ignore_index=True, sort=False)
-df.head()
+    def __idmc_transformer(self):
+        """ IDMC internally displaced persons """
 
-# %%
+        self.idmc['idp'] = self.idmc['Conflict New Displacements'] + self.idmc['Disaster New Displacements']
+        #print("IDMC data source")
+        #print("Data range: {} -> {}".format(idmc.Year.min(), idmc.Year.max()))
+        #print("Data for {} countries.".format(len(idmc.ISO3.unique())))
+        #print("IDP volumes range: {} -> {}".format(idmc.idp.min(), idmc.idp.max()))
+
+        self.idmc = self.idmc.rename(columns={'ISO3': 'Country Code', 
+                                    'Name': 'Country Name', 
+                                    'Year': 'year', 
+                                    'idp': 'value'})
+
+        self.idmc['Indicator Code'] = 'IDMC.IDP'
+        self.idmc['Indicator Name'] = 'Internally displaced persons'
+        self.idmc = self.idmc[['Country Code', 'Country Name', 'year', 'Indicator Code', 'Indicator Name', 'value']]
+
+    def transform(self):
+        """ Transform each of the sources and merge """
+        self.__unhcr_transformer()
+        self.__idmc_transformer()
+
+        # merge
+        self.df = pd.concat([self.unhcr, self.idmc], ignore_index=True, sort=False)
+
