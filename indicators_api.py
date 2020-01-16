80b944cb183fb89db9951c5200f1d7f524262822
Rahul Nair <raul.nair@gmail.com>
2020-01-16 17:27:29 +0000

    metadata on details/sources for indicators

--- indicators_api.py
+++ indicators_api.py
@@ -41,29 +41,78 @@ def fetch_data(config):
     return df
 
 
+def indicator_summary(config):
+    """
+    Generate a summary of indicators, sources etc.
+    """
+
+    result = []
+    for d in config['sources']:
+        tmp = pd.read_csv(os.path.join("..", config['paths']['output'], 
+                            d['name'], 
+                            'data.csv'))
+        
+        sr = (tmp
+            .groupby('Indicator Code')['year']
+            .max()
+            .reset_index()
+            .rename(columns={'year': 'last updated'})
+            .to_dict(orient='records'))
+            
+        # add the source/detail (from config)
+        [s.update({'source': d['source'], 
+                    'detail': d['detail']}) for s in sr]
+    
+        result += sr
+    
+    return result
+
+
 def set_up(app, config):
     
     df = fetch_data(config)
+    metadata = indicator_summary(config)
 
     COUNTRIES = config["supported-countries"]['displacement']
+
+    # Setup some common lookups
+
+    # all countries names/codes in the data
+    country_codes = (df
+            .groupby(['Country Name', 'Country Code'])['year']
+            .count()
+            .reset_index()
+            .drop(columns='year'))
+
+    # all indicators as a key-value store {code: description}
+    grp = (df
+            .groupby(['Indicator Code', 'Indicator Name'])['year']
+            .count()
+            .reset_index()
+            .drop(columns='year')
+            .set_index('Indicator Code').to_dict())
+    INDICATORS = grp['Indicator Name']
+    REV_INDICATORS = {v: k for k, v in INDICATORS.items()}
 
     @app.route("/countries")
     def countries():
         """ Get the list of countries supported """
 
-        # all countries names/codes in the data
-        grp = (df
-                .groupby(['Country Name', 'Country Code'])['year']
-                .count()
-                .reset_index()
-                .drop(columns='year'))
-        idx = grp['Country Code'].isin(COUNTRIES)
-
-        return grp[idx].to_json(orient='records'), 200
+        idx = country_codes['Country Code'].isin(COUNTRIES)
+        return country_codes[idx].to_json(orient='records'), 200
 
     @app.route("/indicators")
     def indicators():
+        """ 
+        Returns all indicator related data for specfic query.
 
+        Query is specified by the following parameters:
+        country: comma separated ISO3 codes or 'all'
+        indicator: indicator code or 'all' for data on all indicators
+        years: a single year e.g. 1980 or a year range, eg. 1980-1990
+
+        Returns a 
+        """
         country = request.args.get('country')
         indicator = request.args.get('indicator')
         years = request.args.get('years')
@@ -92,19 +141,24 @@ def set_up(app, config):
     @app.route("/indicatorCodeByName")
     def indicatorCodeByName():
         indicatorName = request.args.get('indicator')
-        response = df.loc[(df["Indicator Name"] == indicatorName)]["Indicator Code"].unique()[0]
+        try:
+            response = REV_INDICATORS[indicatorName]
+        except KeyError:
+            response = "No indicator '{}' in the data.".format(indicatorName)
+        
         return jsonify(response), 200
 
     @app.route("/uniqueIndicators")
     def uniqueIndicators():
-
-        indicators = df["Indicator Code"].unique()
-        responce = [*indicators]
-        return jsonify(responce), 200
+        """ Returns set of indicator codes in the data """
+        return jsonify(list(INDICATORS.keys())), 200
 
     @app.route("/uniqueIndicatorNames")
     def uniqueIndicatorNames():
+        """ Set of indicator names in the data """
+        return jsonify(list(INDICATORS.values())), 200
 
-        indicators = df["Indicator Name"].unique()
-        responce = [*indicators]
-        return jsonify(responce), 200
+    @app.route("/indicatorMetadata")
+    def indicatorMetadata():
+        """ Return all indicator attributes: source/last updated etc. """
+        return jsonify(metadata), 200
