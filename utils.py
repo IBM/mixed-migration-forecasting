69a0b7350f6a03c16d0ccceac33430f6683537a6
Rahul Nair <raul.nair@gmail.com>
2020-03-04 15:58:30 +0000

    grid search

---
+++ utils.py
@@ -0,0 +1,7 @@
+import argparse
+from utils import gridsearch
+
+if __name__ == "__main__":
+
+    # grid search
+    gridsearch.execute()
--- utils/gridsearch.py
+++ utils/gridsearch.py
@@ -1,12 +1,17 @@
 """
-Determine hyper parameters for SAU model
+Grid search to determine hyper parameters.
 """
 import json
+import numpy as np
 import pandas as pd
 from time import time
-from model import trainer
+from model.displacement import *
+from model.displacement.model import Trainer
+from model.displacement.features import Generator
+
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import FunctionTransformer
+from sklearn.model_selection import TimeSeriesSplit
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -16,7 +21,14 @@ from sklearn.ensemble import GradientBoostingRegressor
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# Things to test
+# Get a data instance
+CONFIGURATION = "configuration.json"
+
+with open(CONFIGURATION, 'rt') as infile:
+    config = json.load(infile)
+
+
+# Model dimensions to determine
 parameters = {
     'Estimator__loss': ['ls', 'lad', 'huber'],
     'Estimator__n_estimators': [10, 50, 100, 150, 200, 300, 350, 400, 500],
@@ -24,71 +36,80 @@ parameters = {
     'Estimator__max_depth': [3, 4, 5, 6]
 }
 
-FSETS = ['ETH', 'SSA',
-         'ETH+AR', 'SSA+AR',
-         'ETH+AR+DEST', 'SSA+AR+DEST',
-         'ETH+AR+DEST+TLAG', 'SSA+AR+DEST+TLAG']
+CLF = Pipeline([("Estimator", GradientBoostingRegressor(random_state=42))])
 
-# Get a data instance
-CONFIGURATION = "configuration.json"
+# Set of parameters for each model
+LAGS = [0, 1, 2, 3, 4, 5]
+COUNTRIES = config['supported-countries']['displacement']
+result = []
 
-with open(CONFIGURATION, 'rt') as infile:
-    config = json.load(infile)
+generator = Generator(config, 2019) # Base year is not used
 
-data, indicators = trainer.get_data(config)
+def get_data(c, lg):
+    """ Custom feature generator """
+
+    # source data
+    df = generator.proj_df.copy(deep=True)
+    
+    if lg > 0:
+        data, varname = generator._Generator__lag_variables(df, TARGETS, lg)
+        true_target_var = varname
+        true_feature_var = generator.indicators[c] + TARGETS
+
+    else:
+        data = df.copy(deep=True)
+        true_target_var = TARGETS
+        true_feature_var= generator.indicators[c]
+
+    c1 = data['Country Code'] == c
+    c2 = ~pd.isnull(data[true_target_var[0]])
+    
+    df = data.loc[c1 & c2, ['year'] + true_feature_var + true_target_var]
+    df = df.sort_values(by=['year'])
+    df = (df.fillna(method='ffill')
+                .fillna(method='bfill'))
+
+    X = df[true_feature_var]
+    y = df[true_target_var]
+    X = X.dropna(axis=1, how='all')
+
+    print("Country: {}, lag: {}".format(c, lg))
+    print("Data has {} rows, {} cols.".format(*df.shape))
+    
+    return X.values, np.ravel(y.values)
+
+def execute():
+    for lg in LAGS:
+        for c in COUNTRIES:
+
+            # get the data
+            X, y = get_data(c, lg)
+
+            # define the splits
+            tscv = TimeSeriesSplit(n_splits=10)
+
+            # do the grid search. Scoring follows the convention that
+            # higher values are better (hence the `neg')
+            grid_search = GridSearchCV(CLF, parameters, scoring='neg_mean_absolute_error',
+                                    cv=tscv.split(X), n_jobs=-1, verbose=1)
+
+            print("Performing grid search...")
+            t0 = time()
+            grid_search.fit(X, y)
+            print("done in %0.3fs" % (time() - t0))
+
+            # record best result
+            par_res = grid_search.best_estimator_.get_params()
+            
+            m = {pn: par_res[pn] for pn in parameters.keys()}
+            m['country'] = c
+            m['lag'] = lg
+            
+            result.append(m)
 
 
-# Run grid search for each instance for each feature set
+    json.dump(result, open("model/displacement/params.json", 'w'))
+    print("Done")
 
-result = {}
-for target_config in trainer.TARGETS:
-    target = target_config['targets']
-
-    for fs in FSETS:
-        # Get the y
-        d = trainer.generate_features(data=data,
-                                      period=(1995, 2017),
-                                      target=target,
-                                      indicators=indicators,
-                                      feature_sets=fs)
-        X, y = d['data']
-
-        clf = Pipeline([("Preprocessor", SimpleImputer(strategy='mean')),
-                        ("Scaler", StandardScaler()),
-                        ("Estimator", GradientBoostingRegressor(n_estimators=100,
-                                                                max_depth=6,
-                                                                learning_rate=0.4,
-                                                                random_state=42,
-                                                                loss='ls'))])
-
-        # Execute the grid search
-        grid_search = GridSearchCV(clf, parameters, scoring='neg_mean_absolute_error',
-                                   cv=5, n_jobs=-1, verbose=1)
-
-        print("Performing grid search...")
-        print("pipeline:", [name for name, _ in clf.steps])
-        print("parameters:")
-        print(parameters)
-        t0 = time()
-        grid_search.fit(X, y)
-        print("done in %0.3fs" % (time() - t0))
-
-        best_score = grid_search.best_score_
-        par_res = grid_search.best_estimator_.get_params()
-        best_parameters = {pn: par_res[pn] for pn in parameters.keys()}
-        print("Best score %s: %0.3f" % (fs, best_score))
-        print("Best parameters set:")
-        for param_name in sorted(parameters.keys()):
-            print("\t%s: %r" % (param_name, best_parameters[param_name]))
-
-        try:
-            _, sc, ofs = result[target]
-            if best_score > sc:
-                result[target] = best_parameters, best_score, fs
-                print("{} did better than {} for {} ".format(fs, ofs, target))
-
-        except KeyError:
-            result[target] = best_parameters, best_score, fs
-
-print("Done")
-print(result)
+if __name__ == "__main__":
+    execute()
