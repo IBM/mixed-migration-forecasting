a94ac19de733955a868b2a8fb72cc8098e1a26a0
Rahul Nair <raul.nair@gmail.com>
2020-04-24 18:22:08 +0100

    fix test

--- server/tests/test_model.py
+++ server/tests/test_model.py
@@ -6,9 +6,6 @@ import numpy as np
 import pytest
 from itertools import product
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 import sys, os
 sys.path.insert(0, os.path.join(os.getcwd()))
 
@@ -26,31 +23,6 @@ config = json.load(open(CONFIGURATION, 'rt'))
 groupings = json.load(open(GROUPINGS, 'rt'))
 
 COUNTRIES = config['supported-countries']['displacement']
-
-def plot_result(tr, pred):
-
-    df = tr.generator.data
-    c1 = df['Country Code'] == pred[0]['country']
-    y = df.loc[c1, 'DRC.TOT.DISP']
-    x = df.loc[c1, 'year']
-
-    plt.figure()
-    plt.plot(x, y, '-b.', markersize=10, label='observation')
-
-    pred_x = [a['year'] for a in pred[0]['prediction']]
-    pred_y = [a['forecast'] for a in pred[0]['prediction']]
-    ci_low = [a['CI_low'] for a in pred[0]['prediction']]
-    ci_high = [a['CI_high'] for a in pred[0]['prediction']]
-
-    plt.plot(pred_x, pred_y, 'rs', label=u'Forecast')
-    plt.fill(np.concatenate([pred_x, pred_x[::-1]]),
-         np.concatenate([ci_high, ci_low[::-1]]),
-         alpha=.5, fc='g', ec='None', label='95% interval')
-    plt.legend(loc=0)
-
-    plt.xlim([1995, config['BASEYEAR']])
-    plt.xticks(np.arange(1995, config['BASEYEAR'] + 8, 1))
-    plt.title(pred[0]['country'])
 
 @pytest.fixture
 def trainer():
